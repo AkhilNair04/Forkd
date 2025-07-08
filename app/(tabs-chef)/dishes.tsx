74b95411458b7af791d26684c443b67911e5768c
NandanaRamesh <nandanaramesh111@gmail.com>
2025-07-08 20:18:32 +0530

    Changes made

---
+++ app/(tabs-chef)/dishes.tsx
@@ -0,0 +1,11 @@
+// app/(tabs-chef)/dishes.tsx
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default function DishesScreen() {
+  return (
+    <View>
+      <Text>Dishes go here</Text>
+    </View>
+  )
+}
